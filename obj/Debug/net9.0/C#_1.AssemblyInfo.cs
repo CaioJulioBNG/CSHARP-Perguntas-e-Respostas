d5edd85782729ced74ddc363820e729b0fd892d1
Caio <caiojulio_bng@outlook.com>
2025-02-23 16:54:21 -0300

    Adicionando Imagem

--- obj/Debug/net9.0/C#_1.AssemblyInfo.cs
+++ obj/Debug/net9.0/C#_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+280e67594cd7591aaa5f83142ff1f2cb3c41d925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7295826428e5907e5e0caac65754fa85471653")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
